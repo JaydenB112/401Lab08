9a45d06a9f76ad53e68c48eaba51d0a05d7a8c87
Jayden Boyd <jaydenboydb1@gmail.com>
2023-08-05 09:49:36 -0500

    Something's wrong

---
+++ ConsoleApp1/Library.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab08
+{
+    internal class Library: ILibrary
+    {
+        private Dictionary<string, Book> Storage;
+
+        public Library() 
+        {
+            Book book1 = new Book("To Kill a MockingBird","Some Weirdo");
+            Book book2 = new Book("War and Peace", "Some war dude, or peace dude, or both.");
+            Book book3 = new Book("")
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Lab8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello welcome to my Library");
+
+            Console.WriteLine("Here are your options:");
+            Console.WriteLine("View all books [1]");
+            Console.WriteLine("Add a Book [2]");
+            Console.WriteLine("Borrow a book [3]");
+            Console.WriteLine("Return a book [4]");
+        }
+}
